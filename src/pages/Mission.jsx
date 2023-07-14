a2146b0e2a134b091d42d97dd18f9633b6fbc9f6
Cyrus Guest <cyrus.t.guest@gmail.com>
2023-07-14 11:49:46 -0400

    mission page setup

--- src/pages/Mission.jsx
+++ src/pages/Mission.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import MobileNav from "../components/MobileNav";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Mission = () => {
-  return <div>About</div>;
+  let { MobileNavOpen } = useContext(AppContext);
+
+  return (
+    <div>
+      {MobileNavOpen ? <MobileNav /> : ""}
+
+      <div className={MobileNavOpen ? "opacity-50" : "opacity-100"}>
+        <Navbar />
+
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default Mission;
