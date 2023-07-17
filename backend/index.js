019d25b3cbd1b94039d702e519d651b8284e7ec5
Cyrus Guest <cyrus.t.guest@gmail.com>
2023-07-17 10:09:44 -0400

    big updats

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const morgan = require("morgan");
+const axios = require("axios");
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, GetCommand } = require("@aws-sdk/lib-dynamodb");
 const cors = require("cors");
 const AmazonCognitoIdentity = require("amazon-cognito-identity-js");
+const {
+  CognitoIdentityProviderClient,
+  GetUserCommand,
+} = require("@aws-sdk/client-cognito-identity-provider");
+const {
+  CloudSearchDomainClient,
+  SearchCommand,
+} = require("@aws-sdk/client-cloudsearch-domain");
 const app = express();
 
 app.use(morgan("dev"));
@@ -14,15 +25,158 @@ const poolData = {
 };
 
 const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+const dynamoDb = new DynamoDBClient({ region: "us-east-1" });
+const ddbDocClient = DynamoDBDocumentClient.from(dynamoDb, {
+  marshallOptions: { removeUndefinedValues: true },
+});
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+app.post("/api/v1/verify_token", async (req, res) => {
+  const token = req.body.token;
+
+  const client = new CognitoIdentityProviderClient({ region: "us-east-1" });
+
+  const params = {
+    AccessToken: token,
+  };
+
+  try {
+    const command = new GetUserCommand(params);
+    const response = await client.send(command);
+
+    // Token is valid, set the user in state or perform any other necessary actions
+    const user = response.UserAttributes;
+    res.json({ statusCode: 200, user });
+  } catch (error) {
+    // Token is invalid or expired, clear the token from local storage
+    res.json({ statusCode: 400, error, user: null });
+  }
+});
+
+app.get("/api/v1/opportunities/:organizationId/:id", async (req, res) => {
+  const organizationId = req.params.organizationId;
+  const id = parseInt(req.params.id);
+  const params = {
+    TableName: "PlatePals",
+    Key: {
+      id,
+      organizationId,
+    },
+  };
+
+  try {
+    const command = new GetCommand(params);
+    const response = await ddbDocClient.send(command);
+
+    if (response.Item) {
+      res.json(response.Item);
+    } else {
+      res.status(404).send("Opportunity not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving opportunity");
+  }
+});
+
+app.get("/api/v1/opportunities", async (req, res) => {
+  // Extract query parameters
+  const { searchTerm, searchLocation, limit = 9, lastEvaluatedKey } = req.query;
+  let latitude;
+  let longitude;
+
+  if (searchLocation !== "") {
+    const apiKey = "AIzaSyAELzQedRkBkX8gYQZYjMg9dMqDmph_9MM";
+
+    await axios
+      .get("https://maps.googleapis.com/maps/api/geocode/json", {
+        params: {
+          address: searchLocation,
+          key: apiKey,
+        },
+      })
+      .then((response) => {
+        const results = response.data.results;
+        if (results.length > 0) {
+          const coordinates = results[0].geometry.location;
+          latitude = coordinates.lat;
+          longitude = coordinates.lng;
+          console.log("Latitude:", latitude);
+          console.log("Longitude:", longitude);
+        } else {
+          console.log("Location not found");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+
+  // Create an instance of the CloudSearchDomainClient
+  const cloudSearchClient = new CloudSearchDomainClient({
+    region: "us-east-1", // Replace with your AWS region
+    endpoint:
+      "https://search-platepals-dno26mtgddk4w77a6qyh3gamlu.us-east-1.cloudsearch.amazonaws.com", // Replace with your CloudSearch endpoint
+  });
+
+  // Construct the search query
+  let searchParams = {
+    query: "matchall",
+    size: parseInt(limit), // Maximum number of items to return
+    start: lastEvaluatedKey ? parseInt(lastEvaluatedKey) : 0, // Pagination offset
+    queryParser: "structured",
+  };
+
+  // Conditionally add the query parameter if searchTerm is not empty
+  if (searchTerm !== "" && searchTerm !== undefined) {
+    console.log("first");
+    searchParams.query = searchTerm;
+    searchParams.queryParser = undefined;
+  }
+
+  if (isNaN(limit)) searchParams.size = 9;
+
+  if (searchLocation !== "" && searchLocation !== undefined) {
+    const exprObj = {
+      distance: `haversin(${latitude}, ${longitude}, coordinates.latitude, coordinates.longitude)`,
+    };
+
+    searchParams.expr = JSON.stringify(exprObj);
+    searchParams.sort = "distance asc";
+    searchParams.return =
+      "distance,city,coordinates,applicants,description,id,organizationid,organizationname,rate,state,title";
+  }
+
+  try {
+    // Send the search command to CloudSearch
+    const searchCommand = new SearchCommand(searchParams);
+    const searchResponse = await cloudSearchClient.send(searchCommand);
+
+    // Extract the search results from the response
+    const items = searchResponse.hits.hit.map((hit) => {
+      const fields = hit.fields;
+      // Convert array values to single elements
+      const item = {};
+      for (const [key, value] of Object.entries(fields)) {
+        item[key] = value[0];
+      }
+      return item;
+    });
+
+    // Extract the LastEvaluatedKey for pagination
+    const lastEvaluatedKey =
+      searchResponse.hits.hit.length > 0
+        ? String(searchResponse.hits.hit[searchResponse.hits.hit.length - 1].id)
+        : null;
+
+    return res.json({ items, lastEvaluatedKey });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Could not load items" });
+  }
 });
 
 app.post("/api/v1/create_user", (req, res) => {
   const userData = req.body;
-
-  console.log(req.body);
 
   let attributeList = [];
 
@@ -75,7 +229,7 @@ app.post("/api/v1/create_user", (req, res) => {
   );
 });
 
-app.post("/api/v1/signin", (req, res) => {
+app.post("/api/v1/signin", async (req, res) => {
   const { email, password } = req.body;
 
   const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(
@@ -92,14 +246,31 @@ app.post("/api/v1/signin", (req, res) => {
   const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
 
   cognitoUser.authenticateUser(authenticationDetails, {
-    onSuccess: (result) => {
-      const accessToken = result.getAccessToken().getJwtToken();
-      res.send({
-        statusCode: 200,
-        user: cognitoUser,
-        accessToken,
-      });
+    onSuccess: async (result) => {
+      const token = result.getAccessToken().getJwtToken();
+      const client = new CognitoIdentityProviderClient({ region: "us-east-1" });
+
+      const params = {
+        AccessToken: token,
+      };
+
+      try {
+        const command = new GetUserCommand(params);
+        const response = await client.send(command);
+
+        // Token is valid, set the user in state or perform any other necessary actions
+        const user = response.UserAttributes;
+        res.send({
+          statusCode: 200,
+          user: user,
+          accessToken: token,
+        });
+      } catch (error) {
+        // Token is invalid or expired, clear the token from local storage
+        res.json({ statusCode: 400, error, user: null });
+      }
     },
+
     onFailure: (err) => {
       res.status(401).send({
         statusCode: 401,
